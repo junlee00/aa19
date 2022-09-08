603115fa23750469b2dd1bcf50e486d7cf9590c0
junlee00 <100752558+junlee00@users.noreply.github.com>
2022-09-08 16:40:31 +0900

    Add files via upload

---
+++ aa19_rpt02/start/hello_function.js
@@ -0,0 +1,7 @@
+//hello function.js
+function hello(what) {
+    console.log("Hello " + what + " ! ");
+}
+
+hello("aa19");
+hello("junlee00, 이현준");
---
+++ aa19_rpt02/start/index_aa19.js
@@ -0,0 +1,7 @@
+//index_aa19.js
+
+var myinfo = require('./aa19info');
+
+myinfo("aa04", "junlee00", '010-52xx-xxxx');
+
+myinfo("aa19", "comsi", '010-52xx-xxxx');
